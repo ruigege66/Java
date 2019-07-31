52be9df4c4042720360b2554f8ee9c8761fcdd9f
ruigege66 <37611248+ruigege66@users.noreply.github.com>
2019-08-01 00:16:55 +0800

    Create d10_2_transferred_meaning_character.java

---
+++ d10_2_transferred_meaning_character.java
@@ -0,0 +1,13 @@
+public class d10_2_transferred_meaning_character
+{
+	public static void main(String[] args) 
+	{	
+		char c1 = 'a';
+		System.out.println(c1);
+		System.out.print(c1);//print代表输出之后不换行，而println代表输出之后换行
+		char c2 = '\n';//转义字符，\n代表换行,反斜杠在java中是转义作用
+		System.out.println(c2);
+		char c3 = '\t';
+		System.out.print(c3);
+	}
+}
