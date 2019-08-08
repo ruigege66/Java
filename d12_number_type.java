806cd5d1619c067a6c18f01b81eb6d9f8a1ff36d
dongqianrui <1215217867@qq.com>
2019-08-09 00:00:36 +0800

    Create d12_number_type.java

---
+++ d12_number_type.java
@@ -0,0 +1,10 @@
+public class d12_number_type{
+    public static void main(String[] args){
+        int i1 = 2147483647;
+        long i2 = 2147483647;
+        long i3 = 2147483648L;
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+    }
+}
