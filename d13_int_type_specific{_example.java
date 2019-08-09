485ffa750b1096e830c45473252cdae8b2f661be
dongqianrui <1215217867@qq.com>
2019-08-09 23:58:25 +0800

    Create d13_int_type_specific{_example.java

---
+++ d13_int_type_specific{_example.java
@@ -0,0 +1,19 @@
+public class d13_{
+    public static void main(String[] args){
+        long i5 = 100L;
+        //int i6 = i5;
+        //??????????????????
+        int i6 = (int)i5;
+        System.out.println(i6);
+
+        long i7 = 2147483648L;
+        int i8 = (int)i7;
+        System.out.println(i8);
+
+        byte i11 = 50;
+        byte i9 = 127;
+        byte i10 = 128;
+
+
+    }
+}
