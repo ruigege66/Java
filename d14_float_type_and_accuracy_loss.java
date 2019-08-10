be2c026599bdc51377cac5ec42d70d30492638ff
dongqianrui <1215217867@qq.com>
2019-08-10 18:04:59 +0800

    Create d14_float_type_and_accuracy_loss.java

---
+++ d14_float_type_and_accuracy_loss.java
@@ -0,0 +1,13 @@
+public class d14_{
+    public static void main(String[] args){
+        int i1 = 128;
+        byte i2 = (byte)i1;
+        System.out.print(i2);
+        
+        double i3 = 23.0;
+        float i4 = 23.0F;
+        System.out.println(i3);
+        System.out.print(i4);
+        
+    }
+}
