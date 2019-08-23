07cb7a0c95608ccdeaa696e5beb25ab4b702004a
dongqianrui <1215217867@qq.com>
2019-08-24 00:26:25 +0800

    Update d20_switch_exercise.java

--- d20_switch_exercise.java
+++ d20_switch_exercise.java
@@ -4,7 +4,7 @@ public class d20_switch_exercise{
 		switch (a) {
 			case 20 :
 				System.out.println("我直接匹配成功了");
-				break;
+				break;//匹配成功了，因为有break后面不会执行了
 			case 12:
 				System.out.println("我匹配失败了");
 				break;
@@ -13,7 +13,7 @@ public class d20_switch_exercise{
 		}
 		switch (a) {
 			case 20 : case 12 : case 15:
-				System.out.println("我是测试多条件的");
+				System.out.println("我是测试多条件的");//这里匹配只要满足一个就行，就能执行下去
 				break;
 			default :
 				System.out.println("我是剩余的语句");
@@ -27,7 +27,7 @@ public class d20_switch_exercise{
 					System.out.println("我匹配失败了");
 				case 29 :
 					System.out.println("我是为了测试case穿透的");
-					break;
+					break;//因为第一个匹配成功了，而且后面没有break语句，会一直执行下去直到这里的break
 				default :
 					System.out.println("我是剩余的语句");
 		}
