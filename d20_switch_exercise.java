3579098239c62ccf013212cef9a25370118beccb
dongqianrui <1215217867@qq.com>
2019-08-24 00:15:28 +0800

    Update d20_switch_exercise.java

--- d20_switch_exercise.java
+++ d20_switch_exercise.java
@@ -2,17 +2,34 @@ public class d20_switch_exercise{
 	public static void main(String[] args) {
 		int a = 10+10;
 		switch (a) {
-		case 20 :
-			println("我直接匹配成功了");
-		case 12:
-			println("我匹配失败了");
-		case 20 : case 12 : case 15:
-			println("我第二次匹配成功了");
-		case 20 :
-			println("我是为了避免case穿透的");
-			break;
-		case 20 :
-			println("我如果打印出来了，代表上面的语句失败了");
+			case 20 :
+				System.out.println("我直接匹配成功了");
+				break;
+			case 12:
+				System.out.println("我匹配失败了");
+				break;
+			default :
+				System.out.println("我是剩余的语句");
+		}
+		switch (a) {
+			case 20 : case 12 : case 15:
+				System.out.println("我是测试多条件的");
+				break;
+			default :
+				System.out.println("我是剩余的语句");
+		}
+		switch (a) {
+				case 20 :
+					System.out.println("我直接匹配成功了");
+				case 12:
+					System.out.println("我匹配失败了");
+				case 18 :
+					System.out.println("我匹配失败了");
+				case 29 :
+					System.out.println("我是为了测试case穿透的");
+					break;
+				default :
+					System.out.println("我是剩余的语句");
 		}
 	}
 }
