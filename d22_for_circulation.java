2a85661a246b3cdf97deec4f2a0c55c2fd6e3aa5
dongqianrui <1215217867@qq.com>
2019-08-29 00:59:42 +0800

    Create d22_for_circulation.java

---
+++ d22_for_circulation.java
@@ -0,0 +1,8 @@
+public class d22_for_circulation{
+	public static void main(String[] args) {
+		//需求：输出数字1~10
+		for (int i = 0;i<=10;++i) {//for循环中之中的变量i的作用域仅限于当前for循环之中使用
+			System.out.println(i);
+		}
+	}
+}
