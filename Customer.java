18e49107795c091f56d1b877264f014cb407883c
dongqianrui <1215217867@qq.com>
2019-10-15 00:56:09 +0800

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.java_learning;
+
+public class Customer{
+	  String name;​
+//	  当张三在购物的时候，输出”张三在购物“
+//	  当李四购物的时候，输出”李四在购物“
+	  public shopping() {
+		  System.out.println(this.mame + "在购物"​);//对象是有名字的，这里的this就是代表对象名
+	  }
+	​}
