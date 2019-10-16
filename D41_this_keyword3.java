a7f78dbb70370a62a7ee40868216cf7b4f085ac8
dongqianrui <1215217867@qq.com>
2019-10-17 00:23:25 +0800

    Update D41_this_keyword3.java

--- D41_this_keyword3.java
+++ D41_this_keyword3.java
@@ -1,1 +1,13 @@
 
+package com.bjpowernode.java_learning;
+
+public class D41_this_keyword3 {
+	
+	public void do1() {
+		System.out.println("youxiu");
+	}
+	public void do2() {
+		System.out.println("youxiu");
+		do1();//编译成功，因为do2调用就必须创建了对象，因此do1这个实例方法肯定能调用
+	}
+}
