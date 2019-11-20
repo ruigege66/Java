bf462744d5e1a708ab626027127b00ca1da53d05
dongqianrui <1215217867@qq.com>
2019-11-21 00:04:09 +0800

    Create D52_1_SingleMode.java

---
+++ D52_1_SingleMode.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.java_learning;
+
+public class D52_1_SingleMode {
+	public static void main(String[] args) {
+		HttpServlet52 h1 = new HttpServlet52();
+	}
+}
+class Servlet52{
+	private Servlet52() {}
+}
+class HttpServlet52 extends Servlet52{
+	
+}
