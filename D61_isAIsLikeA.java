c93c02bd7d0a43e90438cb6d150f6271eba7d6aa
dongqianrui <1215217867@qq.com>
2019-12-17 00:16:49 +0800

    Create D61_isAIsLikeA.java

---
+++ D61_isAIsLikeA.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.java_learning;
+
+public class D61_isAIsLikeA {
+	public static void main(String[] args) {
+		int a = 10;
+		int b =0;
+		if(b!=0) {
+			int c = a/b;
+			System.out.println(a + "/" + b + "=" + c);
+		}else {
+			System.out.println("除数不能为0");
+		}
+	}
+}
