3e92e6736afd3865913ba156b6351b22440dade1
dongqianrui <1215217867@qq.com>
2019-12-26 00:16:07 +0800

    Create D65_2_ArrayExcerse.java

---
+++ D65_2_ArrayExcerse.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.java_learning;
+
+public class D65_2_ArrayExcerse {
+	public static void main(String[] args) {
+		//声明一个一维数组，用来存储int类型
+		int[] a1 = {100,200,300,400};
+		//boolean类型数组
+		boolean a2 = {true,false,true};
+		//String类型数组
+		String[] a3 = {"zhansan","lisi","wangwu"};
+		//Object数组
+		Object o1 = new Object();
+		Object o2 = new Object();
+		Object o3 = new Object();
+		Object[] objs = {o1,o2,o3};
+		
+	}
+}
