f2bcb02a344c712f3fd8e9c943aec7b78554eace
dongqianrui <1215217867@qq.com>
2020-01-19 23:35:17 +0800

    Create D76_2_PackedTypeExample.java

---
+++ D76_2_PackedTypeExample.java
@@ -0,0 +1,9 @@
+package com.bjpowernode.java_learning;
+
+public class D76_2_PackedTypeExample {
+	public static void main(String[] args) {
+		Double d1 = new Double(48);
+		int d2 = d1.intValue();
+		System.out.println(d2);
+	}
+}
