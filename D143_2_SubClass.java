493d3b372d03a2ca347e63172517b3821bc98f9f
dongqianrui <1215217867@qq.com>
2020-10-10 23:51:10 +0800

    Create D143_2_SubClass.java

---
+++ D143_2_SubClass.java
@@ -0,0 +1,9 @@
+package com.bjpowernode.java_learning;
+
+public class D143_2_SubClass extends D143_1_SuperClass{
+	
+	@Override
+	public void MethodB() {
+		System.out.println("子类重写了父类的MethodB方法");
+	}
+}
