e096a213e209993dcf93a1e8d45ea3ee64a7cdad
dongqianrui <1215217867@qq.com>
2021-08-31 00:50:50 +0800

    Create D148_Test.java

---
+++ D148_Test.java
@@ -0,0 +1,13 @@
+
+
+package com.bjpowernode.java_learning;
+
+public class D148_Test {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.name"));
+		System.out.println();
+	}
+}
+
+
