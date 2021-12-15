f513fa31d93a8e260340cea4ba05dbba1e578800
ruigege66 <1215217867@qq.com>
2021-12-16 00:44:37 +0800

    提交HashMap分析代码

--- newJava/src/com/newJava/D151_HashMapAnalysis.java
+++ newJava/src/com/newJava/D151_HashMapAnalysis.java
@@ -1,5 +1,17 @@
 package com.newJava;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
 public class D151_HashMapAnalysis {
+	public static void main(String[] args) {
+		HashMap hashMap = new HashMap();
+		List list1 = new ArrayList();
+		Set set1 = new HashSet();
+	}
 
 }
