22dbde7b3a14ba010600a7e66cf6b7cafaacf420
ruigege66 <1215217867@qq.com>
2021-12-23 00:40:48 +0800

    提交数组IO类的代码

--- newJava/src/com/newJava/D154_InputOutputStream.java
+++ newJava/src/com/newJava/D154_InputOutputStream.java
@@ -1,10 +1,15 @@
 package com.newJava;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class D154_InputOutputStream {
 	public static void main(String[] args) {
@@ -30,7 +35,7 @@ public class D154_InputOutputStream {
 		FileOutputStream fis = null;
 		try {
 			fis = new FileOutputStream(address);
-			fis.write("有点优秀".getBytes());
+			fis.write("有点优秀".getBytes()); // getBytes()获取这个字符串的byte数组，下面的toCharArray()获取字符数组
 			fis.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -45,7 +50,37 @@ public class D154_InputOutputStream {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-			
+		
+		FileWriter fw = null;
+		char[] arargs = "太牛逼了".toCharArray();
+		try {
+			fw = new FileWriter(address);
+			fw.write(arargs, 0, arargs.length);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			byte[] arr = "厉害了".getBytes(StandardCharsets.UTF_8);
+			InputStream is2 = new BufferedInputStream(new ByteArrayInputStream(arr));
+			byte[] flush = new byte[1024];
+			int len = 0;
+			while((len = is2.read(flush)) != -1) {
+				System.out.println(new String(flush, 0, len));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			byte[] info = "厉害了".getBytes();
+			baos.write(info, 0, info.length);
+			byte[] dest = baos.toByteArray();
+			baos.close();
+ 		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
