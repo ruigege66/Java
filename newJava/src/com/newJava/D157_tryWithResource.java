4c3fe0df397ec1a6e4adc931a5db5410ca1df6d6
ruigege66 <1215217867@qq.com>
2022-01-01 16:45:03 +0800

    提交tryQWtihResource

--- newJava/src/com/newJava/D157_tryWithResource.java
+++ newJava/src/com/newJava/D157_tryWithResource.java
@@ -8,10 +8,11 @@ public class D157_tryWithResource {
 	public static void main(String[] args) {
 		String address = "E:\\d05_gitcode\\Java\\newJava\\src\\com\\newJava\\newFile.txt";
 		try (InputStream is = new FileInputStream(address);) {
-			int i = -1;
+			int i = -1;	
 			while ((i = is.read()) != -1) {
 				System.out.println(i);
 			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
